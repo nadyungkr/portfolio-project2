76ea61c52db2059092f9b4703235367df9959a04
nadyungkr <dyungkr@naver.com>
2020-05-20 00:45:08 +0900

    슬라이드 만들기, 버튼 만들기

--- p1.js
+++ p1.js
@@ -84,6 +84,40 @@ $('.mNav').click(function(){
     $('.menu').toggle();
 });
 
+/*image-slide blue section*/
+$(document).ready(function(){
+    var imgs;
+    var img_count;
+    var img_position = 1;
+
+    imgs = $('.main_visual > .visual_container');
+    img_count = imgs.children().length;
+
+    $('.back').click(function(){
+        back();
+    })
+    $('.next').click(function(){
+        next();
+    })
+    
+    function back(){
+        if(1<img_position){
+            imgs.animate({
+                left:'+=100%'
+            });
+            img_position--;
+        }
+    }
+    function next(){
+        if(img_count>img_position){
+            imgs.animate({
+                left:'-=100%'
+            });
+            img_position++;
+        }
+    }
+});
+
 /*responsive*/
 $(window).resize(function(){
     var wWidth = $(window).width();
