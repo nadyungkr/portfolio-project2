f2215cec478de773f244133ad09d038684f88426
nadyungkr <dyungkr@naver.com>
2020-05-29 22:55:40 +0900

    tab menu contents

--- p1.js
+++ p1.js
@@ -148,7 +148,7 @@ function showSlides(n) {
     slides[slideIndex-1].style.display = "block"; 
 }
 
-function tabMenu(evt, tabName) {
+function tabMenu(evt, aboutNum) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
@@ -158,7 +158,7 @@ function tabMenu(evt, tabName) {
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-    document.getElementById(tabName).style.display = "block";
+    document.getElementById(aboutNum).style.display = "block";
     evt.currentTarget.className += " active";
 }
 // Get the element with id="defaultOpen" and click on it
