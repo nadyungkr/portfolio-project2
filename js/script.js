3426e5369da71942f277a7ecdbf4742a2e8d0536
nadyungkr <dyungkr@naver.com>
2020-06-22 23:33:01 +0900

    로드 문법 수정

--- js/script.js
+++ js/script.js
@@ -1,37 +1,31 @@
 /*card_stack*/
-$(document).ready(function(){
-    $(".loader-wrapper").fadeOut("slow");
+$('.card').click(function(){
+    var currentItem = $('.card').filter('.live');
+    var nextItem = currentItem.next();
+    var contentFirst = $('.card').first();
+    var lastItem = $('.card').last();
     
-    var card = $('.card');
-    var currentItem = card.filter('.live');
+    currentItem.removeClass('live');
+    currentItem.addClass('live3');
 
-    card.click(function cardStack(){
-        var nextItem = currentItem.next();
-        var contentFirst = card.first();
-        var lastItem = card.last();
+    if (currentItem.is(lastItem)) {
+        contentFirst.removeClass('live2');
+        currentItem = contentFirst.addClass('live');
+        contentFirst.next().removeClass('live3');
+        contentFirst.next().addClass('live2');
+
+    } else if (currentItem.is(contentFirst)) {
+        currentItem = nextItem.addClass('live');
+        currentItem = nextItem.removeClass('live2');
+        currentItem.next().addClass('live2');
+        currentItem.next().removeClass('live3');
         
-        currentItem.removeClass('live');
-        currentItem.addClass('live3');
-
-        if (currentItem.is(lastItem)) {
-            contentFirst.removeClass('live2');
-            currentItem = contentFirst.addClass('live');
-            contentFirst.next().removeClass('live3');
-            contentFirst.next().addClass('live2');
-
-        } else if (currentItem.is(contentFirst)) {
-            currentItem = nextItem.addClass('live');
-            currentItem = nextItem.removeClass('live2');
-            currentItem.next().addClass('live2');
-            currentItem.next().removeClass('live3');
-          
-        } else {
-            currentItem = nextItem.removeClass('live2');
-            currentItem = nextItem.addClass('live');
-            contentFirst.removeClass('live3');
-            contentFirst.addClass('live2');
-        }
-    });
+    } else {
+        currentItem = nextItem.removeClass('live2');
+        currentItem = nextItem.addClass('live');
+        contentFirst.removeClass('live3');
+        contentFirst.addClass('live2');
+    }
 });
 
 var nav = $("nav ul li"); //메뉴 버튼
